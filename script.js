75e0eb029f00dc13ae021f395d06e3d3bfd7db1d
Ivan Razbegaev <sinnless13+1@gmail.com>
2021-10-01 16:35:14 +0300

    lesson 02 hard one-line solution added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*
+//Решение через перебор циклом
+
 const num = 266219;
 let result = 1;
 let numArray = num.toString().split("");
@@ -6,3 +9,12 @@ for (let i = 0; i < numArray.length; i++){
     result *= numArray[i];
 }
 console.log((result ** 3).toString().slice(0,2));
+
+ */
+
+// Решение "одной" строкой
+const num = 266219;
+console.log( (num.toString().split("").map(Number)
+    .reduce( (previousValue, currentValue) => {
+        return previousValue *= currentValue;
+    })**3 ).toString().slice(0,2));
