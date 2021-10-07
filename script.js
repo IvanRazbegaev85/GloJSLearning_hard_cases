3305a7f38e026a35428168ebc317d7089a82a95e
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-07 23:46:59 +0300

    comments removed фÑand added some work with daysOfWeek obj and user input

--- script.js
+++ script.js
@@ -6,46 +6,45 @@ Lesson 03 hard, case 1
 
 //Решение через объект (ассциативный массив):
 
-// const lang = {
-//     "en": ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-//     "ru": ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-// };
-//
-// console.log(lang.en.join(", "));           //Выводим дни недели на английском строкой
-// console.log(lang.ru.join(", "));           //Выводим дни недели на русском строкой
+let lang = prompt("Please enter your locale (en/ru)", "en");
+
+const daysOfWeek = {
+    "en": ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    "ru": ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+};
+
+if (lang && typeof lang === "string" && lang.toLowerCase() == "en"){
+    console.log(daysOfWeek.en.join(", "));
+} else if (lang && typeof lang === "string" && lang.toLowerCase() == "ru"){
+    console.log(daysOfWeek.ru.join(", "));
+} else console.log("No such locale");
 
 // Решение через "if":
 
-// const lang = "";
-//
-// if (lang === "en"){
-//     console.log('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-// } else if (lang === "ru")
-//     {
-//         console.log('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота');
-//     } else console.log('No such locale');
+if (lang === "en"){
+    console.log(daysOfWeek.en.join(", "));
+} else if (lang === "ru"){
+    console.log(daysOfWeek.ru.join(", "));;
+} else console.log('No such locale');
 
 // Решение через switch()
 
-// const lang = "";
-//
-// switch (true){
-//     case lang === "en":
-//         console.log('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-//         break;
-//     case lang === "ru":
-//         console.log('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота');
-//         break;
-//     default:
-//         console.log("No such locale");
-//         break;
-// }
+switch (true){
+    case lang === "en":
+        console.log(daysOfWeek.en.join(", "));
+        break;
+    case lang === "ru":
+        console.log(daysOfWeek.ru.join(", "));
+        break;
+    default:
+        console.log("No such locale");
+        break;
+}
 
 /*
 Lesson 03 hard, case 2
  */
-
-const namePerson = "";
+let namePerson = prompt("Введите имя: ");
 
 namePerson === "Артем" ? console.log("Директор") :
     namePerson === "Александр" ? console.log("Преподаватель") : console.log("Студент");
